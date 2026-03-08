5b4d7f1b4570a8f799c00f93150167134eb346f1
mboyer3215-debug <m.boyer3215@gmail.com>
2026-03-08 11:18:36 +0100

    Update admin.auth.routes.js

--- backend/routes/admin.auth.routes.js
+++ backend/routes/admin.auth.routes.js
@@ -4,7 +4,7 @@
 const express  = require('express');
 const router   = express.Router();
 const bcrypt   = require('bcryptjs');
-const jwt      = require('jsonwebtoken');
+const jwt      = require('jsonwebtoken'); 
 
 /**
  * POST /api/admin/login
